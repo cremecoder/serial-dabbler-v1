4e1c64a51b4d08082921c353f7dc06ac92ab4669
Sean <sean.cremecoder@gmail.com>
2021-11-09 15:58:07 +0000

    added notion and static props dummy run

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,34 @@
 import Category from "../components/Category"
 import Main from "../styles/Main.styled"
 import { useState, useEffect } from "react"
+import { Client } from "@notionhq/client"
 
-// getStaticProps - theme vars and contentful categories
+const notion = new Client({
+  auth: process.env.NOTION_TOKEN
+})
 
-export default function Home() {
+export async function getStaticProps() {
+  const database_id = process.env.NOTION_DATABASE_ID
+
+  const payload = {
+    path: `databases/${database_id}/query`,
+    method: "POST"
+  }
+
+  const { results } = await notion.request(payload)
+
+  return {
+    props: {
+      listItems: results
+    }
+  }
+}
+
+export default function Home({ listItems }) {
   const [isDesktop, setIsDesktop] = useState(false)
 
   useEffect(() => {
+    console.log(listItems)
     if (window.innerWidth >= 1366) {
       setIsDesktop(true)
     } else {
