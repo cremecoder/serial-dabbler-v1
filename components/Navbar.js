7f870789bce721e8bbd2077f490b9e4400d8770c
Sean <sean.cremecoder@gmail.com>
2021-11-18 11:42:31 +0000

    solved transition

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,34 +1,46 @@
-import { useContext } from "react"
+import { useContext, useState } from "react"
 import { LayoutContext } from "../components/Layout"
 
 import { StyledNav, FlexNav } from "../styles/Navbar.styled"
 import Slide from "../styles/Slide.styled"
 import { AboutButton } from "../styles/Buttons.styled"
 import { useTheme } from "styled-components"
+import { Transition } from "react-transition-group"
 
 const Navbar = () => {
   const theme = useTheme()
   const { width } = useContext(LayoutContext)
+  const [toggle, setToggle] = useState(false)
 
   return (
     <StyledNav>
-      <Slide width={width}>
-        <FlexNav>
-          <h1>
-            SERIAL
-            <br /> DABBLER
-          </h1>
-          <AboutButton clrPrimary={theme.colors.white}>
-            <span>X</span>
-          </AboutButton>
-        </FlexNav>
-      </Slide>
+      <Transition in={toggle} timeout={300}>
+        {state => (
+          <Slide width={width} state={state}>
+            <FlexNav>
+              <h1>
+                SERIAL
+                <br /> DABBLER
+              </h1>
+              <AboutButton
+                onClick={() => setToggle(!toggle)}
+                clrPrimary={theme.colors.white}
+              >
+                <span>X</span>
+              </AboutButton>
+            </FlexNav>
+          </Slide>
+        )}
+      </Transition>
       <FlexNav>
         <h1>
           SERIAL
           <br /> DABBLER
         </h1>
-        <AboutButton clrPrimary={theme.colors.white}>
+        <AboutButton
+          onClick={() => setToggle(!toggle)}
+          clrPrimary={theme.colors.white}
+        >
           <span>ABOUT</span>
         </AboutButton>
       </FlexNav>
--- styles/Slide.styled.js
+++ styles/Slide.styled.js
@@ -1,9 +1,6 @@
-import { useState } from "react"
-
 import styled from "styled-components"
-import { CSSTransition, TransitionGroup } from "react-transition-group"
 
-const StyledSlide = styled.div`
+const Slide = styled.div`
   padding: 0.25em 1.25em;
   position: absolute;
   top: 0;
@@ -16,61 +13,15 @@ const StyledSlide = styled.div`
   padding: 1.25em;
   background-color: ${({ theme }) => theme.colors.black};
   color: ${({ theme }) => theme.colors.white};
-  /* transform: ${props =>
-    props.width >= 1366 ? "translate(-100%, 0)" : "translate(0, -100%)"};
-  transition: transform 300ms ease; */
-
-  .slide-enter {
-    transform: ${props =>
-      props.width >= 1366 ? "translate(-100%, 0)" : "translate(0, -100%)"};
-  }
-
-  .slide-enter-active {
-    transform: translate(0, 0);
-    transition: transform 300ms ease;
-  }
-
-  .slide-exit {
-    transform: translate(0, 0);
-  }
-
-  .slide-exit-active {
-    transform: ${props =>
-      props.width >= 1366 ? "translate(-100%, 0)" : "translate(0, -100%)"};
-    transition: transform 300ms ease;
-  }
-
-  /* .slide-top .slide-content {
-    transform: translate(0, -100%);
-  }
-
-  .slide-left .slide-content {
-    transform: translate(100%, 0);
-  }
-
-  .slide-bottom .slide-content {
-    transform: translate(0, 100%);
-  }
-
-  .slide-right .slide-content {
-    transform: translate(-100%, 0);
-  }  */
-
-  /* :hover div {
-    transform: translate(0, 0);
-  } */
+  transform: ${({ state, width }) =>
+    state === "entering" || state === "entered"
+      ? "translate(0, 0)"
+      : width >= 1366
+      ? "translate(-100%, 0)"
+      : "translate(0, -100%)"};
+  /* transform: ${({ width }) =>
+    width >= 1366 ? "translate(-100%, 0)" : "translate(0, -100%)"}; */
+  transition: transform 300ms ease;
 `
-
-const Slide = ({ children, width }) => {
-  const [toggle, setToggle] = useState(true)
-
-  return (
-    <TransitionGroup component={StyledSlide} width={width}>
-      <CSSTransition in={toggle} timeout={300} classNames="slide">
-        <StyledSlide width={width}>{children}</StyledSlide>
-      </CSSTransition>
-    </TransitionGroup>
-  )
-}
 
 export default Slide
